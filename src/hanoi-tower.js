afad9bc24108a2c0d32b25b60baa667821f8fb90
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 13:42:23 +0100

    feat: solved

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,4 +2,7 @@ const CustomError = require("../extensions/custom-error")
 
 module.exports = function calculateHanoi(diskNumber, turnsSpeed) {
   const turns = Math.pow(2, diskNumber) - 1
+  const seconds = Math.floor((turns / turnsSpeed) * 60 * 60)
+  return { turns, seconds }
+
 }
