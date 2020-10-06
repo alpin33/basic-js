917ad837c6ee86d1e1186b7448ec447897d7f631
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 12:42:10 +0100

    feat: solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,8 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
 module.exports = function countCats(backyard) {
-  const newBackyard = backyard.flat();
-};
+  const newBackyard = backyard.flat()
+  const catsArray = newBackyard.filter((val) => val === "^^")
+  const catsCount = catsArray ? catsArray.length : 0
+  return catsCount
+}
