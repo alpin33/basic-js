be8993478b63f45b6022dd56e53667833ec08cbb
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 13:05:41 +0100

    feat: add const

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-
-  
+  const {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator,
+  } = options
+  let newStr = String(str)
+  let newAddition = ""
 }
   
