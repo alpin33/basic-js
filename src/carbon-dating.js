d4bdc4092eca19df8f7665fbc7241a910bb1da06
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 11:55:52 +0100

    feat: add const

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,4 +5,8 @@ const HALF_LIFE_PERIOD= 5730
 
 module.exports = function dateSample(sampleActivity) {
   const SAMPLE_ACTIVITY = parseFloat(sampleActivity)
+  const rateConstant = 0.693 / HALF_LIFE_PERIOD
+  const elapsedTime =
+    Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY) / rateConstant
+  return Math.ceil(elapsedTime)
 }
