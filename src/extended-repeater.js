68bd171b25b77d0ec746c01fab97577620190bbf
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 13:30:51 +0100

    feat: solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
 module.exports = function repeater(str, options) {
   const {
@@ -8,7 +8,32 @@ module.exports = function repeater(str, options) {
     additionRepeatTimes,
     additionSeparator,
   } = options
+
   let newStr = String(str)
   let newAddition = ""
+
+  if (String(addition) && addition !== undefined) {
+    newAddition = String(addition)
+    if (additionRepeatTimes) {
+      let newAdditionSeparator = "|"
+      if (additionSeparator) {
+        newAdditionSeparator = additionSeparator      }
+      newAddition += newAdditionSeparator;
+      newAddition = newAddition.repeat(additionRepeatTimes)
+      newAddition = newAddition.slice(
+        0,
+        newAddition.length - newAdditionSeparator.length
+      )
+    } }
+
+  if (repeatTimes) {
+    let newSeparator = "+"
+    if (separator) {
+      newSeparator = separator    }
+    newStr = newStr + newAddition + newSeparator
+    newStr = newStr.repeat(repeatTimes)
+    newStr = newStr.slice(0, newStr.length - newSeparator.length)
+  } else {    newStr += newAddition  }
+
+  return newStr
 }
-  
