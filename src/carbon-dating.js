f639d02685e776bf399a71949dc8e7b4ff4cf3b5
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 12:30:14 +0100

    feat: solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,8 +5,16 @@ const HALF_LIFE_PERIOD= 5730
 
 module.exports = function dateSample(sampleActivity) {
   const SAMPLE_ACTIVITY = parseFloat(sampleActivity)
+  if (
+    isNaN(SAMPLE_ACTIVITY) ||
+    SAMPLE_ACTIVITY <= 0 ||
+    SAMPLE_ACTIVITY > 15 ||
+    typeof sampleActivity !== "string"
+  ) {
+    return false
+  }
   const rateConstant = 0.693 / HALF_LIFE_PERIOD
   const elapsedTime =
     Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY) / rateConstant
-  return Math.ceil(elapsedTime)
+  return Math.ceil(elapsedTime)   
 }
