e18992343730896c67a1ac1903b115c24bd8fb35
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 14:40:37 +0100

    feat: add code2

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,17 +6,20 @@ const chainMaker = {
     return this.chain.length
   },
   addLink(value) {
-   
+    this.chain.push(`( ${String(value)} )`)
+    return this
   },
   removeLink(position) {
-    
-  },
-  reverseChain() {
-    
+    if (isNaN(position) || position < 1 || position > this.chain.length) {
+      this.chain = []
+      throw new Error()
+    }
+    this.chain.splice(position - 1, 1)
+    return this
   },
   finishChain() {
    
   }
-};
+}
 
 module.exports = chainMaker;
