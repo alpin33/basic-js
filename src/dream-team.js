15b7533505df61bc23ee603ed18517ea3ad70128
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-06 12:57:46 +0100

    feat: solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,4 +4,11 @@ module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) {
     return false
   }
+  const firstLettersNamesArray = members.map((val) => {
+    if (typeof val === "string") {
+      return val.trim()[0].toUpperCase()
+    }
+  });
+  const secretName = firstLettersNamesArray.sort().join("")
+  return secretName
 }
