f45f04a80b76ac8bccc614949f06e7b8e7ba07c1
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-07 12:37:33 +0100

    feat: solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,6 @@
-const CustomError = require("../extensions/custom-error")
+const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  
-}
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
 class VigenereCipheringMachine {
-  encrypt() {
-    
-  }    
-  decrypt() {
-   
-    
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
-}
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+    }
+    let newMessage = message.toUpperCase()
+    let newKey = key.toUpperCase()
+    let newString = ""
+    let shift = 0
+    for (let i = 0; i < newMessage.length; i++) {
+      if (newMessage.charCodeAt(i) >= 65 && newMessage.charCodeAt(i) <= 90) {
+        const charIndex =
+          (newMessage.charCodeAt(i) -
+            65 +
+            newKey.charCodeAt((i - shift) % newKey.length) -
+            65) %
+          this.alphabet.length;
+        newString += this.alphabet[charIndex]
+      } else {
+        shift++;
+        newString += newMessage[i]
+      }
+    }
+
+    return this.direct ? newString : newString.split("").reverse().join("")
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error()
+    }
+    let newEncryptedMessage = encryptedMessage.toUpperCase()
+    let newKey = key.toUpperCase()
+    let newString = []
+    let shift = 0
+    for (let i = 0; i < newEncryptedMessage.length; i++) {
+      if (
+        newEncryptedMessage.charCodeAt(i) >= 65 &&
+        newEncryptedMessage.charCodeAt(i) <= 90
+      ) {
+        let charIndex =
+          (newEncryptedMessage.charCodeAt(i) -
+            65 -
+            newKey.charCodeAt((i - shift) % newKey.length) -
+            65) %
+          this.alphabet.length
+        if (charIndex < 0) {
+          charIndex += 26
+        }
+        newString += this.alphabet[charIndex]
+      } else {
+        shift++
+        newString += newEncryptedMessage[i]
+      }    }
+    return this.direct ? newString : newString.split("").reverse().join("")
+  }}
 
 module.exports = VigenereCipheringMachine
